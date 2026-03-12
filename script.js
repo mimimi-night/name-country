c60bd42a8ba53d01b17db1cacb6bf5c3c60f1dd4
mimimi-mumumu <beautybloom@me.com>
2026-03-12 16:26:08 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,51 @@
 const japan = [
-"Haruto",
-"Yuto",
-"Sota",
-"Yuki",
-"Hayato"
+"haruto",
+"yuto",
+"sota",
+"yuki",
+"hayato"
 ]
 
 const usa = [
-"Liam",
-"Noah",
-"Oliver",
-"Elijah",
-"James"
+"liam",
+"noah",
+"oliver",
+"elijah",
+"james"
 ]
 
 const korea = [
-"Minjun",
-"Seojun",
-"Jiho",
-"Yujun",
-"Junseo"
+"minjun",
+"seojun",
+"jiho",
+"yujun",
+"junseo"
 ]
+
+const flags = {
+japan:"🇯🇵",
+usa:"🇺🇸",
+korea:"🇰🇷"
+}
 
 function convert(){
 
-let name = document.getElementById("nameInput").value
+let name = document.getElementById("nameInput").value.toLowerCase()
 
 let rank = japan.indexOf(name)
 
 if(rank === -1){
 document.getElementById("result").innerText =
-"name not found"
+""Name not found! Your name might be rare and special!""
 return
 }
 
 let usName = usa[rank]
 let krName = korea[rank]
-
+  
+document.getElementById("flags").innerHTML =
+flags.usa + " " + flags.korea
+  
 document.getElementById("result").innerHTML =
 "USA name: " + usName +
 "<br>Korea name: " + krName
