d83dccf386e6722ec4501f2cd48fd0df74421256
mimimi-mumumu <beautybloom@me.com>
2026-03-13 13:37:06 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,64 @@
+function normalizeName(name){
+
+return name
+.toLowerCase()
+.replaceAll("uu","u")
+.replaceAll("ou","o")
+.replaceAll("oo","o")
+
+}
+
 function convert(){
 
-let name = document.getElementById("nameInput").value.trim().toLowerCase()
+let name = document.getElementById("nameInput").value.trim()
+
 let country = document.getElementById("country").value
-let gender = document.getElementById("gender").value  
-let list = nameData[country]
-let rank = list.indexOf(name)
+
+let gender = document.getElementById("gender").value
+
+
+name = normalizeName(name)
+
+
+let list = nameData[country][gender]
+
+let normalizedList = list.map(n => normalizeName(n))
+
+let rank = normalizedList.indexOf(name)
+
 
 if(rank === -1){
+
 document.getElementById("result").innerText =
 "Name not found! Your name might be rare and special!"
+
 return
+
 }
+
 
 let html = '<div class="result-grid">'
 
+
 for(let key in nameData){
 
-if(key === country){
-continue
-}
+if(key === country) continue
 
-let convertedName = nameData[key][rank]
+let convertedName = nameData[key][gender][rank]
 
-if(convertedName){
 html +=
 '<div class="result-card">' +
 flags[key] + '<br>' +
 countryNames[key] + '<br>' +
-'<b>' + convertedName + '</b>' +
+'<b>'+ convertedName +'</b>' +
 '</div>'
-}
 
 }
+
 
 html += '</div>'
 
+
 document.getElementById("result").innerHTML = html
+
 }
