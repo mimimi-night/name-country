b132fca03e7b0a33e05e44cdaec4ed492d1795de
mimimi-mumumu <beautybloom@me.com>
2026-03-13 13:10:02 +0900

    Create names.js

---
+++ names.js
@@ -0,0 +1,25 @@
+const nameData = {
+japan: [
+"haruto",
+"yuto",
+"sota",
+"yuki",
+"hayato"
+],
+
+usa: [
+"liam",
+"noah",
+"oliver",
+"elijah",
+"james"
+],
+
+korea: [
+"minjun",
+"seojun",
+"jiho",
+"yujun",
+"junseo"
+]
+}
